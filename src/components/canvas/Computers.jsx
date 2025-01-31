a1e4e0f7c1aa425037ce348d2986dfe4f7467cea
biswass101 <biswassnaeemcse@gmail.com>
2025-02-01 03:59:25 +0600

    changed effects on hero computer section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,25 +6,31 @@ import CanvasLoader from '../Loader'
 const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <mesh>
-      <hemisphereLight intensity={3}
-      groundColor="black"/>
-      <pointLight intensity={3}/>
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize = {1024}
-      />
-      <primitive
+    // <mesh>
+    //   <hemisphereLight intensity={3}
+    //   groundColor="black"/>
+    //   <pointLight intensity={3}/>
+    //   <spotLight
+    //     position={[-20, 50, 10]}
+    //     angle={0.12}
+    //     penumbra={1}
+    //     intensity={1}
+    //     castShadow
+    //     shadow-mapSize = {1024}
+    //   />
+    //   <primitive
+    //     object={computer.scene}
+    //     scale = {isMobile ? 0.3 : 0.75}
+    //     position = {isMobile? [0, -1, -0.5]:[0, -3.25, -1.5]}
+    //     rotaton = {[-0.01, -0.2, -0.1]}
+    //   />
+    // </mesh>
+    <primitive
         object={computer.scene}
         scale = {isMobile ? 0.3 : 0.75}
         position = {isMobile? [0, -1, -0.5]:[0, -3.25, -1.5]}
         rotaton = {[-0.01, -0.2, -0.1]}
       />
-    </mesh>
   )
 }
 
